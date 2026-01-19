cd6cf7701ad0d8fc212e86dba0341ec276089595
Arya Miryala <aryamiryala5@gmail.com>
2026-01-18 22:17:01 -0800

    updating webscraper for selenium to capture dynamic content

--- AryaMiryala_9567678120/lab1/scripts/web_scraper.py
+++ AryaMiryala_9567678120/lab1/scripts/web_scraper.py
@@ -1,25 +1,25 @@
 import time
 import os
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.service import Service
+
 
 url = "https://www.cnbc.com/world/?region=world"
 
 def scrape_data():
     print("Setting up Headless Chrome Driver...")
     
-    # configuarations
     chrome_options = Options()
     chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     
+
+    service = Service("/usr/bin/chromedriver")
+    
+    driver = None
     try:
-        # initialize the driver
-        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
         
         print(f"Loading {url}...")
@@ -28,21 +28,19 @@ def scrape_data():
         print("Waiting 5 seconds for dynamic content...")
         time.sleep(5)
         
-        # get the html
         page_source = driver.page_source
-        
-        # save to file
         file_path = os.path.join("..", "data", "raw_data", "web_data.html")
+        
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(page_source)
             
         print(f"Success! Saved rendered HTML to {file_path}")
-
+        
     except Exception as e:
         print(f"Error: {e}")
         
     finally:
-        if 'driver' in locals():
+        if driver:
             driver.quit()
 
 if __name__ == "__main__":
